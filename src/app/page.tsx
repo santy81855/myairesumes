0b0ce5442a13b0bfc04bb8bf0b189de3c48a3c73
santy <santy@DESKTOP-7KIGB8O>
2024-01-28 22:13:41 -0500

    added nav directly to landing page. Working on setting upa esume nimation to loop seamlessly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import styles from "./page.module.css";
 import Title from "@/components/landing-page/title/Title";
 import Blur from "@/components/landing-page/color-blur/Blur";
 import HeroImage from "@/components/landing-page/hero-image/HeroImage";
+import Navbar from "@/components/nav/Navbar";
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <section className={styles.heroSection}>
+                <Navbar />
                 <section className={styles.heroSectionText}>
                     <Title />
                 </section>
--- src/components/landing-page/hero-image/HeroImage.tsx
+++ src/components/landing-page/hero-image/HeroImage.tsx
@@ -1,6 +1,5 @@
 import styles from "./HeroImage.module.css";
 import Image from "next/image";
-import Resume from "@/components/landing-page/resume/Resume";
 import Carousel from "@/components/landing-page/image-carousel/Carousel";
 
 const HeroImage = () => {
