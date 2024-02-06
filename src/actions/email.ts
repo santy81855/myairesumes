f571711ca4b846d44e0f63f172463318e25d5ac4
santy <santy@DESKTOP-7KIGB8O>
2024-02-06 18:20:15 -0500

    test

--- src/actions/email.ts
+++ src/actions/email.ts
@@ -1,55 +1,53 @@
 "use server";
 import { AuthEmailData } from "@/types";
-import axios from "axios";
+import { EmailVerificationTemplate } from "@/components/email-templates/email-verification-template/EmailTemplate";
+import { PasswordResetTemplate } from "@/components/email-templates/password-reset-template/EmailTemplate";
+import { FailedPaymentTemplate } from "@/components/email-templates/payment-failed-template/EmailTemplate";
+import { Resend } from "resend";
 
 export async function send(data: AuthEmailData) {
     const { email, firstName, lastName, subject, url, type } = data;
     console.log("APP DOMAIN: ", process.env.APP_DOMAIN);
     // convert the axios request below to a fetch request
+    const resend = new Resend(process.env.RESEND_API_KEY);
     try {
-        const response = await fetch(`${process.env.APP_DOMAIN}/api/send`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
-            },
-            body: JSON.stringify({
-                email,
-                firstName,
-                lastName,
-                subject,
-                url,
-                type,
-            }),
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log("Email sent: ", data);
-            return data;
-        } else {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+        var template;
+        var from = "";
+        if (type === "email-verification") {
+            template = EmailVerificationTemplate({
+                firstName: firstName,
+                lastName: lastName,
+                url: url,
+            });
+            from = "MyAiResumes <email-verification@myairesumes.com>";
         }
-    } catch (error) {
-        console.error("Email error: ", error);
-        return error;
-    }
-    return;
-    await axios
-        .post(`${process.env.APP_DOMAIN}/api/send`, {
-            email,
-            firstName,
-            lastName,
-            subject,
-            url,
-            type,
-        })
-        .then((res) => {
-            console.log("Email sent: ", res);
-            return res;
-        })
-        .catch((err) => {
-            console.log("Email error: ", err);
-            return err;
+        if (type === "password-reset") {
+            template = PasswordResetTemplate({
+                firstName: firstName,
+                lastName: lastName,
+                url: url,
+            });
+            from = "MyAiResumes <password-reset@myairesumes.com>";
+        }
+        if (type === "failed-payment") {
+            template = FailedPaymentTemplate({
+                firstName: firstName,
+                lastName: lastName,
+            });
+            from = "MyAiResumes <payments@myairesumes.com>";
+        }
+        const data = await resend.emails.send({
+            from: from,
+            to: [email],
+            subject: subject,
+            react: template,
+            text: "",
         });
+        return { success: "Email sent" };
+    } catch (error) {
+        console.log("ERROR");
+        console.log(error);
+        console.log("resend key: ", process.env.RESEND_API_KEY);
+        return { error };
+    }
 }
