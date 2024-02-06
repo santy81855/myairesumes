792b1a4c2717a5362813b0fbeb381874aa4c2fb9
santy <santy@DESKTOP-7KIGB8O>
2024-02-06 17:45:26 -0500

    test

--- src/actions/email.ts
+++ src/actions/email.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 
 export async function send(data: AuthEmailData) {
     const { email, firstName, lastName, subject, url, type } = data;
+    console.log("APP DOMAIN: ", process.env.APP_DOMAIN);
     await axios
         .post(`${process.env.APP_DOMAIN}/api/send`, {
             email,
