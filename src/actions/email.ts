a668f02fe7f2af069a2cc8a54cb7d18e4e4c9cc6
santy <santy@DESKTOP-7KIGB8O>
2024-02-06 17:48:16 -0500

    test

--- src/actions/email.ts
+++ src/actions/email.ts
@@ -5,6 +5,30 @@ import axios from "axios";
 export async function send(data: AuthEmailData) {
     const { email, firstName, lastName, subject, url, type } = data;
     console.log("APP DOMAIN: ", process.env.APP_DOMAIN);
+    // convert the axios request below to a fetch request
+    await fetch(`${process.env.APP_DOMAIN}/api/send`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email,
+            firstName,
+            lastName,
+            subject,
+            url,
+            type,
+        }),
+    })
+        .then((res) => {
+            console.log("Email sent: ", res);
+            return res;
+        })
+        .catch((err) => {
+            console.log("Email error: ", err);
+            return err;
+        });
+    return;
     await axios
         .post(`${process.env.APP_DOMAIN}/api/send`, {
             email,
