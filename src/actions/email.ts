891f5753decfda3c5df9f8e7640f88c7e7312a10
santy <santy@DESKTOP-7KIGB8O>
2024-02-06 18:00:55 -0500

    test

--- src/actions/email.ts
+++ src/actions/email.ts
@@ -6,28 +6,33 @@ export async function send(data: AuthEmailData) {
     const { email, firstName, lastName, subject, url, type } = data;
     console.log("APP DOMAIN: ", process.env.APP_DOMAIN);
     // convert the axios request below to a fetch request
-    await fetch(`${process.env.APP_DOMAIN}/api/send`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email,
-            firstName,
-            lastName,
-            subject,
-            url,
-            type,
-        }),
-    })
-        .then((res) => {
-            console.log("Email sent: ", res);
-            return res;
-        })
-        .catch((err) => {
-            console.log("Email error: ", err);
-            return err;
+    try {
+        const response = await fetch(`${process.env.APP_DOMAIN}/api/send`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                firstName,
+                lastName,
+                subject,
+                url,
+                type,
+            }),
         });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Email sent: ", data);
+            return data;
+        } else {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error("Email error: ", error);
+        return error;
+    }
     return;
     await axios
         .post(`${process.env.APP_DOMAIN}/api/send`, {
