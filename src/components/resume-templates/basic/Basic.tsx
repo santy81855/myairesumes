50ad1751705f3a3b4d305548193a54661c992974
santy <santy@DESKTOP-7KIGB8O>
2024-02-17 13:23:00 -0500

    progress on the basic resume template

--- src/components/resume-templates/basic/Basic.tsx
+++ src/components/resume-templates/basic/Basic.tsx
@@ -1,5 +1,5 @@
 "use client";
-//import styles from "./Basic.module.css";
+import styles from "./Basic.module.css";
 import { useState, useEffect, useRef } from "react";
 import { Inter, Montserrat, Poppins } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -69,8 +69,8 @@ const Basic = ({ download }: ResumeProps) => {
      * Times-Roman
      */
 
-    // Create styles
-    const styles = StyleSheet.create({
+    // Create pdfstyles
+    const pdfstyles = StyleSheet.create({
         document: {
             width: "100%",
             height: "100%",
@@ -79,13 +79,64 @@ const Basic = ({ download }: ResumeProps) => {
             width: "100%",
             height: "100%",
         },
-        view: {
-            display: "flex",
+        pageContainer: {
             backgroundColor: "white",
             width: "100%",
             height: "100%",
             fontSize: fontSize,
             fontFamily: "Times-Roman",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: fontSize,
+        },
+        sectionContainer: {
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingInline: margin,
+            gap: fontSize / 4,
+        },
+        sectionTitle: {
+            fontSize: fontSize * 1.3,
+            fontWeight: "bold",
+        },
+        name: {
+            fontSize: fontSize * 2,
+            fontWeight: "bold",
+            paddingTop: margin,
+        },
+        title: {
+            fontSize: fontSize * 1.2,
+            fontWeight: "light",
+        },
+        contact: {
+            fontSize: fontSize,
+        },
+        rowContainer: {
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: fontSize / 1.5,
+        },
+        bullet: {
+            width: fontSize / 3,
+            height: fontSize / 3,
+            backgroundColor: "black",
+            borderRadius: "50%",
+        },
+        horizontalLine: {
+            width: "100%",
+            height: fontSize / 10,
+            backgroundColor: "black",
+        },
+        contentText: {
+            fontSize: fontSize,
         },
         text: {
             fontSize: fontSize * 1.5,
@@ -95,11 +146,88 @@ const Basic = ({ download }: ResumeProps) => {
         },
     });
 
+    const contactSection = (
+        <View style={pdfstyles.sectionContainer} id="contact">
+            <Text style={pdfstyles.name}>Santiago Garcia</Text>
+            <Text style={pdfstyles.title}>Business Analyst</Text>
+            <view style={pdfstyles.rowContainer}>
+                <Text style={pdfstyles.contact}>(678) 735-9580</Text>
+                <view style={pdfstyles.bullet}></view>
+                <Text style={pdfstyles.contact}>santy@santiagogarcia.dev</Text>
+            </view>
+        </View>
+    );
+
+    const summarySection = (
+        <View style={pdfstyles.sectionContainer} id="summary">
+            <Text style={pdfstyles.sectionTitle}>Summary</Text>
+            <View style={pdfstyles.horizontalLine}></View>
+            <Text style={pdfstyles.contentText}>
+                Experienced professional with diverse skills in leadership,
+                communication, and problem-solving. Proven track record of
+                achieving goals and driving results. Strong team player with
+                excellent organizational abilities. Experienced professional
+                with diverse skills in leadership, communication, and
+                problem-solving. Proven track record of achieving goals and
+                driving results. Strong team player with excellent
+                organizational abilities.
+            </Text>
+        </View>
+    );
+
+    const skillSection = (
+        <View style={pdfstyles.sectionContainer} id="skills">
+            <Text style={pdfstyles.sectionTitle}>Skills</Text>
+            <View style={pdfstyles.horizontalLine}></View>
+            <Text style={pdfstyles.contentText}>
+                Leadership, Communication, Teamwork, Problem-solving, Time
+                management, Adaptability, Creativity, Technical proficiency,
+                Analytical skills, Customer service.
+            </Text>
+        </View>
+    );
+
+    const handleMouseEnter = (e: any) => {
+        const divElement = document.getElementById("section");
+        if (!divElement) return;
+        divElement.style.backgroundColor = "red";
+    };
+
+    const handleMouseLeave = (e: any) => {
+        const divElement = document.getElementById("section");
+        if (!divElement) return;
+        divElement.style.backgroundColor = "white";
+    };
+
     return (
         <Document title="Resume">
-            <Page wrap={false} style={styles.page}>
-                <View style={styles.view} ref={templateRef}>
-                    <Text style={styles.text}>
+            <Page wrap={false} style={pdfstyles.page}>
+                <View style={pdfstyles.pageContainer} ref={templateRef}>
+                    <div
+                        id="section"
+                        className={styles.sectionContainer}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        {contactSection}
+                    </div>
+                    <div
+                        id="section"
+                        className={styles.sectionContainer}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        {summarySection}
+                    </div>
+                    <div
+                        id="section"
+                        className={styles.sectionContainer}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        {skillSection}
+                    </div>
+                    <Text style={pdfstyles.text}>
                         tho auntoheau tnaoehu toehuntshetunho easntuh asontedhu
                         tnaoeh utnaoeh utnshaoesnutaoheuaoedutnshaoe utnshaoe
                         utnshoae uoaedu aoetuh aoeudaoe
@@ -111,10 +239,10 @@ const Basic = ({ download }: ResumeProps) => {
             </Page>
         </Document>
         /*
-        <main className={`${styles.container} ${inter.className}`}>
-            <section className={styles.template} ref={templateRef}>
+        <main className={`${pdfstyles.container} ${inter.className}`}>
+            <section className={pdfstyles.template} ref={templateRef}>
                 <p
-                    className={styles.text}
+                    className={pdfstyles.text}
                     style={{
                         fontSize: 1.5 * fontSize,
                         marginTop: margin,
