dcb3e59f7a0f2495fbd217cd89c918898b5abd9e
santy <santy@DESKTOP-7KIGB8O>
2024-03-10 01:23:56 -0500

    updated the jobs, resume, and cover letter cards at the top of the dashboard

--- src/features/dashboard/components/cards/title-card/Card.tsx
+++ src/features/dashboard/components/cards/title-card/Card.tsx
@@ -1,5 +1,13 @@
+"use client";
 import styles from "./Card.module.css";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import {
+    jobIcon,
+    resumeIcon,
+    coverLetterIcon,
+} from "@/components/icons/iconSVG";
 
 type CardProps = {
     title: string;
@@ -10,6 +18,14 @@ type CardProps = {
 };
 
 const Card = ({ title, number, url, gridArea }: CardProps) => {
+    const [hover, setHover] = useState(false);
+
+    const initialText = (
+        <>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.number}>{number}</p>
+        </>
+    );
     return (
         <Link
             href={`/dashboard?menu=${title.toLowerCase().split(" ").join("-")}`}
@@ -17,13 +33,33 @@ const Card = ({ title, number, url, gridArea }: CardProps) => {
             className={styles.container}
             style={{
                 gridArea,
-                background: `url(${url})`,
-                backgroundSize: "cover",
+                backgroundImage: `url(${url})`,
+                backgroundSize: "contain",
                 backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
             }}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
         >
-            <h3 className={styles.title}>{title}</h3>
-            <p className={styles.number}>{number}</p>
+            <motion.div className={styles.overlayTouch}>
+                <motion.p className={styles.textTouch}>{title}</motion.p>
+                <motion.p className={styles.numberTextTouch}>{number}</motion.p>
+                {title === "Jobs" && jobIcon}
+                {title === "Resumes" && resumeIcon}
+                {title === "Cover Letters" && coverLetterIcon}
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: hover ? 1 : 0 }}
+                transition={{ duration: 0.2 }}
+                className={styles.overlay}
+            >
+                <motion.p className={styles.text}>{title}</motion.p>
+                <motion.p className={styles.numberText}>{number}</motion.p>
+                {title === "Jobs" && jobIcon}
+                {title === "Resumes" && resumeIcon}
+                {title === "Cover Letters" && coverLetterIcon}
+            </motion.div>
         </Link>
     );
 };
