cf9ce649846fb58ee715d55349e81c4a6042e998
santy <santy@DESKTOP-7KIGB8O>
2024-05-22 22:51:48 -0400

    fixed edit buttons not working for contact, work, and education info in profile page

--- src/features/dashboard/components/profile/work/Work.tsx
+++ src/features/dashboard/components/profile/work/Work.tsx
@@ -40,180 +40,201 @@ const Work = ({ currentUser, searchParams }: WorkProps) => {
 
     return (
         <Card key="work-card" gridArea="work" title="Work History">
-            {edit ? (
-                <section className={styles.workInfoContainer}>
-                    <p className={styles.addTitle}>
-                        {workId ? "Edit Job" : "Add a Job"}
-                    </p>
-                    <section className={styles.workInfo}>
-                        <label htmlFor="company" className={styles.label}>
-                            Company
-                        </label>
-                        <input
-                            id="company"
-                            type="text"
-                            name="company"
-                            className={styles.input}
-                            defaultValue={workId ? workBeingEdited.company : ""}
-                            required
-                            autoFocus
-                        />
-                    </section>
-                    <section className={styles.workInfo}>
-                        <label htmlFor="position" className={styles.label}>
-                            Position
-                        </label>
-                        <input
-                            id="position"
-                            type="text"
-                            name="position"
-                            className={styles.input}
-                            defaultValue={
-                                workId ? workBeingEdited.position : ""
-                            }
-                            required
-                        />
-                    </section>
-                    <section className={styles.workInfoHorizontal}>
-                        <p className={styles.label}>Current Employer</p>
-                        <div className={styles.toggleSwitch}>
-                            <Toggle
-                                name="currentEmployer"
-                                defaultChecked={
-                                    workId
-                                        ? workBeingEdited.currentEmployer
-                                        : false
-                                }
-                            />
-                        </div>
-                    </section>
-                    <section className={styles.dateInput}>
+            <form className={styles.formContainer}>
+                {edit ? (
+                    <section className={styles.workInfoContainer}>
+                        <p className={styles.addTitle}>
+                            {workId ? "Edit Job" : "Add a Job"}
+                        </p>
                         <section className={styles.workInfo}>
-                            <label htmlFor="startDate" className={styles.label}>
-                                Start Date
+                            <label htmlFor="company" className={styles.label}>
+                                Company
                             </label>
                             <input
-                                id="startDate"
-                                type="date"
-                                name="startDate"
+                                id="company"
+                                type="text"
+                                name="company"
                                 className={styles.input}
                                 defaultValue={
-                                    workId
-                                        ? workBeingEdited.startDate
-                                        : new Date().toISOString().split("T")[0]
+                                    workId ? workBeingEdited.company : ""
+                                }
+                                required
+                                autoFocus
+                            />
+                        </section>
+                        <section className={styles.workInfo}>
+                            <label htmlFor="position" className={styles.label}>
+                                Position
+                            </label>
+                            <input
+                                id="position"
+                                type="text"
+                                name="position"
+                                className={styles.input}
+                                defaultValue={
+                                    workId ? workBeingEdited.position : ""
                                 }
                                 required
                             />
                         </section>
-                        <section className={styles.workInfo}>
-                            <label htmlFor="endDate" className={styles.label}>
-                                End Date
-                            </label>
-                            <input
-                                id="endDate"
-                                type="date"
-                                name="endDate"
-                                className={styles.input}
-                                defaultValue={
-                                    workId
-                                        ? workBeingEdited.endDate.toLowerCase() ===
-                                          "present"
-                                            ? new Date()
+                        <section className={styles.workInfoHorizontal}>
+                            <p className={styles.label}>Current Employer</p>
+                            <div className={styles.toggleSwitch}>
+                                <Toggle
+                                    name="currentEmployer"
+                                    defaultChecked={
+                                        workId
+                                            ? workBeingEdited.currentEmployer
+                                            : false
+                                    }
+                                />
+                            </div>
+                        </section>
+                        <section className={styles.dateInput}>
+                            <section className={styles.workInfo}>
+                                <label
+                                    htmlFor="startDate"
+                                    className={styles.label}
+                                >
+                                    Start Date
+                                </label>
+                                <input
+                                    id="startDate"
+                                    type="date"
+                                    name="startDate"
+                                    className={styles.input}
+                                    defaultValue={
+                                        workId
+                                            ? workBeingEdited.startDate
+                                            : new Date()
                                                   .toISOString()
                                                   .split("T")[0]
-                                            : workBeingEdited.endDate
-                                        : new Date().toISOString().split("T")[0]
-                                }
-                                max={new Date().toISOString().split("T")[0]}
-                            />
-                        </section>
-                    </section>
-                    <section className={styles.buttonContainer}>
-                        <Link
-                            href="/dashboard?menu=profile"
-                            className={styles.cancelButton}
-                        >
-                            cancel
-                        </Link>
-                        {workId ? (
-                            <section
-                                className={styles.horizontalButtonContainer}
-                            >
-                                <button
-                                    type="submit"
-                                    className={styles.removeButton}
-                                    formAction={removeWorkInfo}
+                                    }
+                                    required
+                                />
+                            </section>
+                            <section className={styles.workInfo}>
+                                <label
+                                    htmlFor="endDate"
+                                    className={styles.label}
                                 >
-                                    Remove
-                                </button>
+                                    End Date
+                                </label>
+                                <input
+                                    id="endDate"
+                                    type="date"
+                                    name="endDate"
+                                    className={styles.input}
+                                    defaultValue={
+                                        workId
+                                            ? workBeingEdited.endDate.toLowerCase() ===
+                                              "present"
+                                                ? new Date()
+                                                      .toISOString()
+                                                      .split("T")[0]
+                                                : workBeingEdited.endDate
+                                            : new Date()
+                                                  .toISOString()
+                                                  .split("T")[0]
+                                    }
+                                    max={new Date().toISOString().split("T")[0]}
+                                />
+                            </section>
+                        </section>
+                        <section className={styles.buttonContainer}>
+                            <Link
+                                href="/dashboard?menu=profile"
+                                className={styles.cancelButton}
+                            >
+                                cancel
+                            </Link>
+                            {workId ? (
+                                <section
+                                    className={styles.horizontalButtonContainer}
+                                >
+                                    <button
+                                        type="submit"
+                                        className={styles.removeButton}
+                                        formAction={removeWorkInfo}
+                                    >
+                                        Remove
+                                    </button>
+                                    <button
+                                        type="submit"
+                                        className={styles.saveButton}
+                                        formAction={updateWorkInfo}
+                                    >
+                                        Update
+                                    </button>
+                                </section>
+                            ) : (
                                 <button
                                     type="submit"
                                     className={styles.saveButton}
-                                    formAction={updateWorkInfo}
+                                    formAction={addWorkInfo}
                                 >
-                                    Update
+                                    Save
                                 </button>
-                            </section>
-                        ) : (
-                            <button
-                                type="submit"
-                                className={styles.saveButton}
-                                formAction={addWorkInfo}
-                            >
-                                Save
-                            </button>
-                        )}
+                            )}
+                        </section>
                     </section>
-                </section>
-            ) : (
-                <section className={styles.workGrid}>
-                    {work.map((job: any, index: number) => {
-                        return (
-                            <section className={styles.workItem} key={index}>
-                                <section className={styles.workItemInfo}>
-                                    <p className={styles.company}>
-                                        {job.company}
-                                    </p>
-                                    <p className={styles.position}>
-                                        {job.position}
-                                    </p>
-                                    <section className={styles.dateRange}>
-                                        <p className={styles.startDate}>
-                                            {formatDateMonthYear(job.startDate)}
-                                            {" - "}
-                                        </p>
-                                        {job.currentEmployer ? (
-                                            <p className={styles.endDate}>
-                                                {job.endDate}
-                                            </p>
-                                        ) : (
-                                            <p className={styles.endDate}>
-                                                {formatDateMonthYear(
-                                                    job.endDate
-                                                )}
-                                            </p>
-                                        )}
-                                    </section>
-                                </section>
-                                <Link
-                                    className={styles.editButton}
-                                    href={`/dashboard?menu=profile&addJob=true&jobId=${job.id}`}
+                ) : (
+                    <section className={styles.workGrid}>
+                        {work.map((job: any, index: number) => {
+                            return (
+                                <section
+                                    className={styles.workItem}
+                                    key={index}
                                 >
-                                    Edit
-                                </Link>
-                            </section>
-                        );
-                    })}
-                    <Link
-                        title="Add a Job"
-                        href="/dashboard?menu=profile&addJob=true"
-                        className={styles.addWorkButton}
-                    >
-                        <div className={styles.svgContainer}>{plusIcon}</div>
-                    </Link>
-                </section>
-            )}
-            <FormLoading />
+                                    <section className={styles.workItemInfo}>
+                                        <p className={styles.company}>
+                                            {job.company}
+                                        </p>
+                                        <p className={styles.position}>
+                                            {job.position}
+                                        </p>
+                                        <section className={styles.dateRange}>
+                                            <p className={styles.startDate}>
+                                                {formatDateMonthYear(
+                                                    job.startDate
+                                                )}
+                                                {" - "}
+                                            </p>
+                                            {job.currentEmployer ? (
+                                                <p className={styles.endDate}>
+                                                    {job.endDate}
+                                                </p>
+                                            ) : (
+                                                <p className={styles.endDate}>
+                                                    {formatDateMonthYear(
+                                                        job.endDate
+                                                    )}
+                                                </p>
+                                            )}
+                                        </section>
+                                    </section>
+                                    <Link
+                                        className={styles.editButton}
+                                        href={`/dashboard?menu=profile&addJob=true&jobId=${job.id}`}
+                                    >
+                                        Edit
+                                    </Link>
+                                </section>
+                            );
+                        })}
+                        <Link
+                            title="Add a Job"
+                            href="/dashboard?menu=profile&addJob=true"
+                            className={styles.addWorkButton}
+                        >
+                            <div className={styles.svgContainer}>
+                                {plusIcon}
+                            </div>
+                        </Link>
+                    </section>
+                )}
+                <FormLoading />
+            </form>
         </Card>
     );
 };
