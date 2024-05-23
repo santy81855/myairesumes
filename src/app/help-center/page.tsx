a15489c961aa9e604572cb351c70afd520a07c70
santy <santy@DESKTOP-7KIGB8O>
2024-05-23 02:41:42 -0400

    bug fix for help-center page

--- src/app/help-center/page.tsx
+++ src/app/help-center/page.tsx
@@ -4,7 +4,7 @@ import { Footer } from "@/features/footer";
 import { faqIcon, videoIcon } from "@/components/icons/iconSVG";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { FaqComponent, Tutorials } from "@/features/help-center";
+//import { FaqComponent, Tutorials } from "@/features/help-center";
 
 const Home = async ({
     params,
@@ -55,9 +55,7 @@ const Home = async ({
                     </section>
                 </section>
             </section>
-            <section className={styles.contentSection}>
-                {option === "tutorials" && <Tutorials />}
-            </section>
+            <section className={styles.contentSection}></section>
             <section className={styles.footerSection}>
                 <Footer />
             </section>
